ecd147b229404e2677fe0cd37273c0083f5f7f2f
devicewhite <devicewhite@proton.me>
2024-10-19 17:48:38 -0300

    test

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+mkdir -p ~/.termux
+curl https://raw.githubusercontent.com/devicewhite/Termux-Style/refs/heads/DeviceWhite/colors.properties > ~/.termux/colors.properties
+exit
