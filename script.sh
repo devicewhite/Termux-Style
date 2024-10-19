d0bb007ce6aa27275448bb7311ff5e9e4a7b6ca6
devicewhite <devicewhite@proton.me>
2024-10-19 17:52:09 -0300

    test

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
 mkdir -p ~/.termux
-curl https://raw.githubusercontent.com/devicewhite/Termux-Style/refs/heads/DeviceWhite/colors.properties > ~/.termux/colors.properties
-exit
+curl -s https://raw.githubusercontent.com/devicewhite/Termux-Style/refs/heads/DeviceWhite/colors.properties > ~/.termux/colors.properties
+echo -e '\e[32mTema instalado com sucesso\nReinicie o termux!'
